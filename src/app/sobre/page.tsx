89b646fa044746af1e417fe1dd6b552199b25fb1
joaofelima96 <jf-lima1996@hotmail.com>
2024-10-03 19:06:21 -0300

    aula2: app-router

--- src/app/sobre/page.tsx
+++ src/app/sobre/page.tsx
@@ -5,10 +5,13 @@ export const metadata: Metadata = {
   description: "Essa é a página sobre",
 };
 
-export default function sobrePage() {
+export default function SobrePage() {
   return (
-    <div>
-      <h2>Sobre</h2>
-    </div>
+    <main>
+      <h1>Sobre</h1>
+      <h2 style={{ margin: "1600px 0" }} id="empresa">
+        A Empresa
+      </h2>
+    </main>
   );
 }
