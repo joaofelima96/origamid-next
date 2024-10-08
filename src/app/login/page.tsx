15de880f45852086a93a5b508c9ed348d398a865
joaofelima96 <jf-lima1996@hotmail.com>
2024-10-08 18:02:55 -0300

    route handlers

---
+++ src/app/login/page.tsx
@@ -0,0 +1,11 @@
+import FormLogin from "@/components/FormLogin/FormLogin";
+
+export default async function LoginPage() {
+  return (
+    <main>
+      <h1>Login</h1>
+
+      <FormLogin />
+    </main>
+  );
+}
--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,6 +1,25 @@
+import { cookies } from "next/headers";
 import Link from "next/link";
 
-export default function Menu() {
+export default async function Menu() {
+  let conta = {
+    autorizado: false,
+    usuario: "",
+  };
+
+  const token = cookies().get("token")?.value;
+
+  const response = await fetch("https://api.origamid.online/conta/perfil", {
+    method: "GET",
+    headers: {
+      Authorization: "Bearer " + token,
+    },
+  });
+
+  if (response.ok) {
+    conta = await response.json();
+  }
+
   return (
     <ul className="menu">
       <li>
@@ -14,6 +33,9 @@ export default function Menu() {
       </li>
       <li>
         <Link href="/cursos">Cursos</Link>
+      </li>
+      <li>
+        {conta.autorizado ? conta.usuario : <Link href="/login">Login</Link>}
       </li>
     </ul>
   );
