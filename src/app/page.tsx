42e3b6a8fb513a474759a178c3b23236bf769219
joaofelima96 <jf-lima1996@hotmail.com>
2025-04-11 16:47:48 -0300

    script

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
+import Script from "next/script";
+
 export default function Home() {
   return (
     <div>
       <main>
         <h1>Home</h1>
+        <Script
+          strategy="beforeInteractive"
+          id="script-teste"
+          src="https://api.origamid.online/scripts/idade-legal.min.js"
+        ></Script>
       </main>
     </div>
   );
